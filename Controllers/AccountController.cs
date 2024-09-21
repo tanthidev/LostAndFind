4976253bc1ec127a160b070a2c8ca49c1785c8fc
Tan Thi <52000806@student.tdtu.edu.vn>
2024-09-21 22:38:09 +0700

    Code UI login/ signup page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LostAndFind.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login() 
+        { 
+            return View(); 
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
